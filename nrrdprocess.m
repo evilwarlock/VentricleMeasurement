009e59a3a62f09ab9dac0c970832dc38f4d54422
evilwarlock <ceaxyz@gmail.com>
2016-02-18 22:39:59 -0500

    bool in cone init

--- nrrdprocess.m
+++ nrrdprocess.m
@@ -1,14 +1,15 @@
-% load volume data
-% clc; 
-% clear;
-% load volume_image.mat;
-% %% visualize skull density from 700 to 3000
+%% load volume data
+clc; 
+clear;
+close all;
+load volume_image;
+%% visualize skull density from 700 to 3000
 % for i = 1 : 20
 %     figure();
 %     imshow(vol1(:,:,i*5+1),[700,3000]);
 % end
 
-% %% visualize ventricles from -5 to 5
+%% visualize ventricles from -5 to 5
 % for i = 1 : 20
 %     figure();
 %     imshow(vol1(:,:,i*5+1),[-5,5]);
@@ -18,49 +19,71 @@
  matVolume = volume_image;
 
 
-% initial Bone and Ventricle mat
-matBone = zeros(512,512,120);
-matVen = zeros(512,512,120);
-pcBone = ones(1,3);
-pcBoneSurf = ones(1,3);
-pcVen = ones(1,3);
-cntBone = 0;
-cntVen = 0;
+%% initial Bone and Ventricle mat
+% matBone = zeros(512,512,120);
+% matVen = zeros(512,512,120);
+% pcBone = ones(1,3);
+% pcBoneSurf = ones(1,3);
+% pcVen = ones(1,3);
+% cntBone = 0;
+% cntVen = 0;
 
-% filter by density bone = 950~1900, ventricles -15 to 15
-for i = 1:512
-    for j = 1:512
-        for k = 1:120
-            if matVolume(i,j,k) >950 && matVolume(i,j,k) < 1950
-                matBone(i,j,k) = 1;
-                cntBone = cntBone + 1;
-%                 if cntBone == 1
-%                     pcBone = [i j k];
-% %                     pcBoneSurf = [i j k];
-% %                     break;  % break to get skull surface 
+%% filter by density bone = 950~1900, ventricles -15 to 15
+% for i = 1:512
+%     for j = 1:512
+%         for k = 1:120
+%             if matVolume(i,j,k) >950 && matVolume(i,j,k) < 1950
+%                 matBone(i,j,k) = 1;
+%                 cntBone = cntBone + 1;
+% %                 if cntBone == 1
+% %                     pcBone = [i j k];
+% % %                     pcBoneSurf = [i j k];
+% % %                     break;  % break to get skull surface 
+% %                 else
+% %                     pcBone = [pcBone ; i j k];
+% % %                     pcBoneSurf = [pcBoneSurf ; i j k];
+% % %                     break;  % break to get skull surface 
+% %                 end
+%             elseif matVolume(i,j,k) > -50 && matVolume(i,j,k) < 50
+%                 matVen(i,j,k) = 1;
+%                 cntVen = cntVen + 1;
+%                 if cntVen == 1
+%                     pcVen = [i j k];
 %                 else
-%                     pcBone = [pcBone ; i j k];
-% %                     pcBoneSurf = [pcBoneSurf ; i j k];
-% %                     break;  % break to get skull surface 
+%                     pcVen = [pcVen ; i j k];
 %                 end
-            elseif matVolume(i,j,k) > -50 && matVolume(i,j,k) < 50
-                matVen(i,j,k) = 1;
-                cntVen = cntVen + 1;
-                if cntVen == 1
-                    pcVen = [i j k];
-                else
-                    pcVen = [pcVen ; i j k];
-                end
-            end
-        end
-    end
-end
- 
-% % visualize bone and ventricle
-% pcshow(pcBoneSurf);
-% pcshow(pcVen);
+%             end
+%         end
+%     end
+% end
 
-% overlap volume calculation
+%%  load calculated bone and ventricle.
+load('pcBoneSurf_unfiltered.mat');
+load('pcVen_unfiltered.mat');
+
+%% filter noisy points for x > 400, y < 150 & y > 400
+minThrx = 0;
+maxThrx = 400;
+minThry = 150;
+maxThry = 400;
+TF1 = pcVen(:,1) <= minThrx & pcVen(:,1) >= maxThrx;
+TF2 = pcVen(:,2) <= minThry & pcVen(:,2) >= maxThry;
+
+% combine them
+TFall = TF1 | TF2;
+
+% remove
+pcVen(TFall,:) = []; 
+
+save pcVen_150_400.mat pcVen;
+ 
+%%  visualize bone and ventricle
+figure();
+pcshow(pcBoneSurf);
+figure();
+pcshow(pcVen);
+
+%% overlap volume calculation
 % [mat1, mat2] = maxOverlapVolume(mat1, mat2)
 
 % for i = 1:512
